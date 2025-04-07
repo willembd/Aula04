88effc64d6eb3458230f43804c4786740a1be00f
Willemberg Bruno <willem.bruno@hotmail.com>
2025-04-07 09:46:03 -0300

    exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,28 @@
+num = int(input("Digite um número de 1 á 12: "))
+
+if num > 12 or num < 1:
+    print("Mês invalido!!")
+elif num == 1:
+    print("Janeiro")
+elif num == 2:
+    print("Fevereiro")
+elif num == 3:
+    print("Março")
+elif num == 4:
+    print("Abril")
+elif num == 5:
+    print("Maio")
+elif num == 6:
+    print("Junho")
+elif num == 7:
+    print("Julho")
+elif num == 8:
+    print("Agosto")
+elif num == 9:
+    print("Setembro")
+elif num == 10:
+    print("Outubro")
+elif num == 11:
+    print("Novembro")
+elif num == 12:
+    print("Dezembro")
